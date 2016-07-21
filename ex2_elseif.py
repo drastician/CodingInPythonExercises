294add97a58e0083e5134cffa09e0830d1b7698a
drastician <naplesr@si.edu>
2016-07-21 12:31:28 -0400

    done
    oops, forgot to commit. here’s the last iteration.

---
+++ ex2_elseif.py
@@ -0,0 +1,44 @@
+# This program will act as a basic calculator. It will ask the user for 2
+# integers, and an operation to perform.
+
+# First, we use the input() function to request for 2 numbers and an operation.
+operation = input("Enter whether you want to add, subtract, multiply, or divide: ")
+first_number = input("Please enter the first number you wish to " + operation + ":")
+second_number = input("Please enter the second number you wish to " + operation + ":")
+
+# Next, we check that both of the numbers are actually integers.
+if first_number.isnumeric() and second_number.isnumeric():
+    # Next, we must convert both of these numbers to integers, since input() 
+    # defaults to accepting strings.
+    first_number = int(first_number)
+    second_number = int(second_number)
+else:
+    print("ERROR: no. you entered crap. only enter integers.")
+    exit()
+
+
+if operation == 'add':
+    answer = first_number + second_number
+
+elif operation == 'subtract':
+    answer = first_number - second_number
+
+elif operation == 'multiply':
+    answer = first_number * second_number
+
+elif operation == 'divide':
+    answer = first_number / second_number
+
+else:
+    answer = 'Missed your chance. You did not enter the appropriate operator'
+
+print(answer)
+
+    ### You need to fill in the rest of the operations, and print an error if
+    ### their entry for operation isn't from our list.
+
+    ### You also need to figure out the correct location to report back the 
+    ### answer.
+
+
+### You need to print an error message if their numbers are not integers.
